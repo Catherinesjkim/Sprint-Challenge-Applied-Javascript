152aa1efb98dc1b4d2bfad7105bb2b9c220b7e91
Catherine Kim <catherinesjkim@gmail.com>
2019-11-10 18:18:21 -0800

    Add Updated Components Header JS File Because: - Looked through the HTML code and familiarized myself with the different sections.
    This commit:
    - Followed the instructions in the Header Index JS file and created the Header component.

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,34 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const container = document.querySelector('.header-container');
 
-function Header() {}
+function Header() {
+  // Define new elements
+  const header = document.createElement('div');
+  const aDate = document.createElement('span');
+  const aTitle = document.createElement('h1');
+  const aTemp = document.createElement('span');
+
+  // Map over the header, creating a compnent for each object and add each component to the DOM as children of the 'header' div
+
+  // Set up structure of elements
+  header.append(aDate, aTitle, aTemp);
+
+  // Set text content
+  aDate.textContent = `MARCH 28, 2019`;
+  aTitle.textContent = `Lambda Times`;
+  aTemp.textContent = `98°`;
+
+  // Style
+  header.classList.add('header');
+  aDate.classList.add('date');
+  aTitle.classList.add('title');
+  aTemp.classList.add('temp');
+
+  return header
+}
+
+const newHeader = Header();
+  container.appendChild(newHeader);
+
